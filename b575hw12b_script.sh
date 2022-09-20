09f2f42a29d55e8f4eb794260efaa60823cac3b3
Zezhong Wan <112365454+Harliy666@users.noreply.github.com>
2022-09-19 20:48:41 -0400

    Add files via upload
    Question B Problem 3

---
+++ b575hw12b_script.sh
@@ -0,0 +1,8 @@
+echo "#== SCRIPT STARTED"
+awk 'NR % 4 == 2' sample.fastq > sample_rseq.txt
+awk 'NR % 4 == 2' sample_cutadapt.fastq > sample_cutadapt_rseq.txt
+echo "#== RETRIEVE READ SEQUENCE DONE"
+sed 's/AGATCGGAAGA.*//' sample_rseq.txt > sample_rseq_CUT.txt
+echo "#== CUT ADAPTER DONE"
+diff sample_rseq.txt sample_rseq_CUT.txt
+echo "#== SCRIPT DONE"
